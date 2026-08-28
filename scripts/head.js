c996c5529e07fea0f9188c3c5ec229939a97078d
Nick Walton <contact@croakingkero.com>
2026-08-28 13:17:24 -0500

    Moved goatcounter from header to head. oopsie

--- scripts/head.js
+++ scripts/head.js
@@ -2,4 +2,5 @@ document.currentScript.insertAdjacentHTML('beforebegin',`
 <title>The Golden Path of Game Programming</title>
 <link href="/style.css" rel="stylesheet">
 <link rel="icon" href="/favicon.ico">
+<script data-goatcounter="https://ultiman3rd.goatcounter.com/count" async src="//gc.zgo.at/count.js"></script>
 `);
